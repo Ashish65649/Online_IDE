41d7d67d19daa676771090fa287340629291b728
Ashish Ranjan <60938080+Ashish65649@users.noreply.github.com>
2023-08-13 12:26:23 +0530

    Update IdeController.java

--- src/main/java/com/online/ide/controller/IdeController.java
+++ src/main/java/com/online/ide/controller/IdeController.java
@@ -2,7 +2,7 @@ package com.online.ide.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
